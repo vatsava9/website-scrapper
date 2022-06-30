3f3a3c434145c69584fc7f541eac08ed6328e493
vatsava <36201503+vatsava9@users.noreply.github.com>
2022-06-30 16:42:36 +0000

    Extracted video detatis

--- scratch.py
+++ scratch.py
@@ -15,15 +15,40 @@ def get_driver():
   driver = webdriver.Chrome(options = chrome_options)
   return driver
 
+def get_videos(driver):
+  VIDEO_DIV_TAG = 'ytd-video-renderer'
+  driver.get(youtube_trending_url)
+  #time.sleep(5)
+  videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
+  return videos
+
+def parse_video(video):
+  title_tag = video.find_element(By.ID, 'video-title')
+  title = title_tag.text
+  url = title_tag.get_attribute('href')
+  
+  thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+
+  channel_div = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
+  channel_name = channel_div.text
+  
+  description = video.find_element(By.ID, 'description-text').text
+
+  return {
+    'title': title,
+    'url': url,
+    'thumbnail_url': thumbnail_url,
+    'channel': channel_name,
+    'description': description
+  }
+
 if __name__ == '__main__':
   print('Creating driver')
   driver = get_driver()
 
   print('Fetching page')
-  driver.get(youtube_trending_url)
-  # print('Page title: ', driver.title)
+  videos = get_videos(driver)
+  print(f'Found {len(videos)} videos')
 
-  print('Get video divs')
-  video_div_class = 'style-scope ytd-video-renderer'
-  video_divs = driver.find_elements(By.video_div_class)
-  print(f'Found{len(video_divs)} videos')
+  print(parse_video(videos[0]))
